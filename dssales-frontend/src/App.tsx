b21839a3d4dfce1b0c12bf5294c546e1027b4e89
Djalves424 <djalves424@gmail.com>
2024-08-22 18:00:52 -0300

    Add PieChartCard component

--- dssales-frontend/src/App.tsx
+++ dssales-frontend/src/App.tsx
@@ -2,7 +2,8 @@ import './App.css';
 import Filter from "./components/filter";
 import Header from "./components/header";
 import SalesByDate from './components/sales-by-date';
-import SalesSummary from './components/sales-summary'
+import SalesSummary from './components/sales-summary';
+import PieChartCard from './components/pie-chart-card';
 
 function App() {
 
@@ -14,6 +15,8 @@ function App() {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PieChartCard name="Lojas" labels={['Uberlândia', 'Araguari', 'Uberaba']} series={[40, 30, 30]} />
+          <PieChartCard name="Pagamento" labels={['Crédito', 'Débito', 'Dinheiro']} series={[20, 50, 30]} />
         </div>
       </div>
     </>
--- dssales-frontend/src/components/sales-summary/index.tsx
+++ dssales-frontend/src/components/sales-summary/index.tsx
@@ -1,3 +1,22 @@
+// import './styles.css';
+// import SalesSummaryCard from './sales-summary-card';
+// import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
+// import { ReactComponent as BarChartIcon } from '../../assets/bar-chart-icon.svg';
+// import { ReactComponent as DoneIcon } from '../../assets/done-icon.svg';
+// import { ReactComponent as SyncIcon } from '../../assets/sync-icon.svg';
+//
+// export default function SalesSummary() {
+//   return (
+//     <div className="sales-sumamry-container">
+//       <SalesSummaryCard value={430} label="Média" icon={<DoneIcon />} />
+//       <SalesSummaryCard value={630} label="Quantidade" icon={<SyncIcon />} />
+//       <SalesSummaryCard value={130} label="Mínima" icon={<BarChartIcon />} />
+//       <SalesSummaryCard value={230} label="Máxima" icon={<AvatarIcon />} />
+//     </div>
+//   );
+// }
+
+
 import './styles.css';
 import SalesSummaryCard from './sales-summary-card';
 import AvatarIcon from '../../assets/avatar-icon.svg';
@@ -5,43 +24,23 @@ import BarChartIcon from '../../assets/bar-chart-icon.svg';
 import DoneIcon from '../../assets/done-icon.svg';
 import SyncIcon from '../../assets/sync-icon.svg';
 
+const summaryData = [
+  { value: 430, label: "Média", icon: DoneIcon },
+  { value: 630, label: "Quantidade", icon: SyncIcon },
+  { value: 130, label: "Mínima", icon: BarChartIcon },
+  { value: 230, label: "Máxima", icon: AvatarIcon },
+];
 export default function SalesSummary() {
   return (
-    <div className="sales-sumamry-container">
-      <SalesSummaryCard value={430} label="Média" icon={<DoneIcon />} />
-      <SalesSummaryCard value={630} label="Quantidade" icon={<SyncIcon />} />
-      <SalesSummaryCard value={130} label="Média" icon={<BarChartIcon />} />
-      <SalesSummaryCard value={230} label="Média" icon={<AvatarIcon />} />
+    <div className="sales-summary-container">
+      {summaryData.map((data, index) => (
+        <SalesSummaryCard
+          key={index}
+          value={data.value}
+          label={data.label}
+          icon={<img src={data.icon} alt={data.label} />}
+        />
+      ))}
     </div>
   );
 }
-
-
-// import './styles.css';
-// import SalesSummaryCard from './sales-summary-card';
-// import AvatarIcon from '../../assets/avatar-icon.svg';
-// import BarChartIcon from '../../assets/bar-chart-icon.svg';
-// import DoneIcon from '../../assets/done-icon.svg';
-// import SyncIcon from '../../assets/sync-icon.svg';
-//
-// const summaryData = [
-//   { value: 430, label: "Média", icon: DoneIcon },
-//   { value: 630, label: "Quantidade", icon: SyncIcon },
-//   { value: 130, label: "Média", icon: BarChartIcon },
-//   { value: 230, label: "Média", icon: AvatarIcon },
-// ];
-//
-// export default function SalesSummary() {
-//   return (
-//     <div className="sales-summary-container">
-//       {summaryData.map((data, index) => (
-//         <SalesSummaryCard
-//           key={index}
-//           value={data.value}
-//           label={data.label}
-//           icon={<img src={data.icon} alt={data.label} />}
-//         />
-//       ))}
-//     </div>
-//   );
-// }
