a259a5f4a1b5647a7b4a2c1701d41e9d0dc5f774
Djalves424 <djalves424@gmail.com>
2024-08-22 16:11:07 -0300

    Making some adjustments to the sales-summary

--- dssales-frontend/src/components/sales-summary/index.tsx
+++ dssales-frontend/src/components/sales-summary/index.tsx
@@ -1,9 +1,9 @@
 import './styles.css';
 import SalesSummaryCard from './sales-summary-card';
-import { ReactComponent as AvatarIcon } from '../../assets/avatar-icon.svg';
-import { ReactComponent as BarChartIcon } from '../../assets/bar-chart-icon.svg';
-import { ReactComponent as DoneIcon } from '../../assets/done-icon.svg';
-import { ReactComponent as SyncIcon } from '../../assets/sync-icon.svg';
+import AvatarIcon from '../../assets/avatar-icon.svg';
+import BarChartIcon from '../../assets/bar-chart-icon.svg';
+import DoneIcon from '../../assets/done-icon.svg';
+import SyncIcon from '../../assets/sync-icon.svg';
 
 export default function SalesSummary() {
   return (
@@ -15,3 +15,33 @@ export default function SalesSummary() {
     </div>
   );
 }
+
+
+// import './styles.css';
+// import SalesSummaryCard from './sales-summary-card';
+// import AvatarIcon from '../../assets/avatar-icon.svg';
+// import BarChartIcon from '../../assets/bar-chart-icon.svg';
+// import DoneIcon from '../../assets/done-icon.svg';
+// import SyncIcon from '../../assets/sync-icon.svg';
+//
+// const summaryData = [
+//   { value: 430, label: "Média", icon: DoneIcon },
+//   { value: 630, label: "Quantidade", icon: SyncIcon },
+//   { value: 130, label: "Média", icon: BarChartIcon },
+//   { value: 230, label: "Média", icon: AvatarIcon },
+// ];
+//
+// export default function SalesSummary() {
+//   return (
+//     <div className="sales-summary-container">
+//       {summaryData.map((data, index) => (
+//         <SalesSummaryCard
+//           key={index}
+//           value={data.value}
+//           label={data.label}
+//           icon={<img src={data.icon} alt={data.label} />}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
